ce3dd32c57c9bc0217c3a10803105f56d0c26ca8
abellugtujr <abelmanuellugtujr1996@gmail.com>
2023-09-23 19:05:50 +0800

    Add project files.

---
+++ BrokeredMessaging.Receiver/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string ConnectionString = "Endpoint=sb://brokeredmessagingsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gn0+loDQPc6oVBgfAeQ8FeUep8pVdMnuP+ASbFU/kOo=";
+string QueueName = "demoqueue";
+
+// Create a service bus client
+var client = new ServiceBusClient(ConnectionString);
+
+// Create a service bus receiver
+var receiver = client.CreateReceiver(QueueName);
+
+// Receive the messages
+Console.WriteLine("Receive messages...");
+
+while (true)
+{
+    var message =await receiver.ReceiveMessageAsync();
+
+    if(message != null)
+    {
+        Console.Write(message.Body.ToString());
+
+        // Complete the message
+        await receiver.CompleteMessageAsync(message);
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("All messages received.");
+        break;
+    }
+};
+
+// Close the receiver
+await receiver.CloseAsync();
---
+++ BrokeredMessaging.Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string ConnectionString = "Endpoint=sb://brokeredmessagingsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=gn0+loDQPc6oVBgfAeQ8FeUep8pVdMnuP+ASbFU/kOo=";
+string QueueName = "demoqueue";
+
+string Sentence = "Microsoft Azure Service Bus.";
+
+// Create a service bus client
+var client = new ServiceBusClient(ConnectionString);
+
+// Create a service bus sender
+var sender = client.CreateSender(QueueName);
+
+// Send some messages
+Console.WriteLine("Sending messages...");
+foreach(var character in Sentence)
+{
+    var message = new ServiceBusMessage(character.ToString());
+    await sender.SendMessageAsync(message);
+    Console.WriteLine($"    Sent:{ character }");
+}
+
+// Close the sender
+await sender.CloseAsync();
+
+
+Console.WriteLine("Sent messages.");
+Console.ReadLine();
